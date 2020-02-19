571acceac79bf613101f4798f28d21edd7a7f797
Gino <ginocote@gmail.com>
2020-02-19 17:34:59 -0500

    smaller simpler

--- src/index.js
+++ src/index.js
@@ -1,70 +1,40 @@
-/*! virtual-storage v1.0.0-beta.1 | MIT */
-module.exports = init();
-//'use strict';
+/*! virtual-storage 0.1.5-beta.1 | MIT */
+'use strict';
 
-  function init() {
+/**
+ * Virtual storage
+ * @param {string} name
+ * @param {object} [{title:'Title 1', description:'Descriptions 1'}]
+ * @return {string}
+ */
 
-    'use strict';
-    let json = {};
+module.exports = vStorage();
 
-    function set(key, value, attributes) {
+function vStorage() {
+  let vStorage = {};
 
-			// IF IS NOT ARRAY MAKE IT A OBJECT
-      // https://stackoverflow.com/a/20956445/211324
-      if(!Array.isArray(value))
-				value = [value];
-      // GC
-      json[key] = value;
-      console.log(json);
-      //printLog(JSON.stringify(json))
-
-      return (json[key] = value);
-      //return (document.cookie = key + '=' + value + stringifiedAttributes)
-    }
-
-    function get(key) {
-      /*   if (typeof document === 'undefined') {
-          return
-        } */
-
-      // To prevent the for loop in the first place assign an empty array
-      // in case there are no cookies at all.
-      if (!json.hasOwnProperty(key)) {
-        console.log("no data found for " + key);
-        //printLog("no data found for " + key);
-        //return "Unavailable";
-        return ;
-      }
-
-      console.log(json);
-      //printLog(JSON.stringify(json[key]));
-      //test = JSON.parse(data[0][name]);
-      //alert(newName)
-      //console.log(data);
-      console.log(json[key][0].title);
-      return json[key][0];
-
-      //return key ? jar[key] : jar
-    }
-    function remove(key) {
-
-    	if(!key)
-      	json = {};
-    	else if (key)
-      	delete json[key];
-
-      //printLog(JSON.stringify(json));
-      console.log(json);
-      return json;
-
-      //return key ? jar[key] : jar
-    }
-
-    return Object.create({
-      set: set,
-      get: get,
-      remove: remove
-    });
+  function set(key, value) {
+    Array.isArray(value)||(value=[value]);
+    vStorage[key] = value;
+    return (vStorage[key] = value);
   }
 
-//export default init();
+  function get(key) {
+    if (!vStorage.hasOwnProperty(key)) {
+      console.log("no data found for " + key);
+      return ;
+    }
+    console.log(vStorage);
+    return vStorage[key][0];
+  }
+  function remove(key) {
+    key ? delete vStorage[key] : vStorage = {} ;
+    return vStorage;
+  }
+
+  return Object.create({
+    set: set,
+    get: get,
+    remove: remove
+  });
+}
