b4500403110290df744efddbf8138280a491f447
Gino <ginocote@gmail.com>
2020-02-19 17:41:53 -0500

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-// cd C:\GC\1-node-modules\my-module\node-js-server
-// node index.js
-var vStorage = require('test');
-
-var data_name2 = vStorage.set('data_name2', [{title:'Title 2', description:'Descriptions 2'}]);
-console.log(data_name2);
-
-var get_data_name2 = vStorage.get('data_name3');
-console.log(get_data_name2);
