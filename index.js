7f182267967dc437940e0e0d7fe4cdda9bfb1731
Gino <ginocote@gmail.com>
2020-03-05 18:19:52 -0500

    Update smaller

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*! virtual-storage 0.1.5-beta.1 | MIT */
+/*! virtual-storage 0.1.8-beta.1 | MIT */
 'use strict';
 
 /**
@@ -11,23 +11,21 @@
 module.exports = vStorage();
 
 function vStorage() {
-  let vStorage = {};
+  let vStorage = [];
 
   function set(key, value) {
-    Array.isArray(value)||(value=[value]);
     vStorage[key] = value;
     return (vStorage[key]);
-    // return (vStorage[key] = value);
   }
   function get(key) {
     if (!vStorage.hasOwnProperty(key)) {
       console.log("no data found for " + key);
       return vStorage ;
     }
-    return vStorage[key][0];
+    return vStorage[key];
   }
   function remove(key) {
-    key ? delete vStorage[key] : vStorage = {} ;
+    key ? delete vStorage[key] : vStorage = [] ;
     return vStorage;
   }
   return Object.create({
