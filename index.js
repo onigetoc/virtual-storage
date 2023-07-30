6784bca8e3f2185a18e0e74d7a169a8f432f2688
Gino <ginocote@gmail.com>
2023-07-30 15:43:19 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -10,18 +10,31 @@
 
 module.exports = vStorage();
 
+// const sessionID = new Date().getTime();
+// const sessionID = '_' + Math.random().toString(36).substr(2, 9);
+
+// const sessionID = function () {
+//   // Math.random should be unique because of its seeding algorithm.
+//   // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+//   // after the decimal.
+//   return '_' + Math.random().toString(36).substr(2, 9);
+// };
+
 function vStorage() {
   let vStorage = [];
 
   function set(key, value) {
+    // Array.isArray(value)||(value=[value]);
     vStorage[key] = value;
     return (vStorage[key]);
+    // return (vStorage[key] = value);
   }
   function get(key) {
     if (!vStorage.hasOwnProperty(key)) {
       console.log("no data found for " + key);
       return vStorage ;
     }
+    // return vStorage[key][0];
     return vStorage[key];
   }
   function remove(key) {
